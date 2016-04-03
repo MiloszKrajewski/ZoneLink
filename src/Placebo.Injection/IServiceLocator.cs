a26b089104eb59acc5aa9ea166c38c3f177cb824
Milosz Krajewski <miloszkrajewski@o2.pl>
2016-04-03 22:35:47 +0100

    Squashed 'src/Placebo/' changes from 03858ef..e428bb2
    e428bb2 added registration to IServiceLocator
    
    git-subtree-dir: src/Placebo
    git-subtree-split: e428bb2b6d1c305c966b8d10021e8a6506e9e06b

--- src/Placebo.Injection/IServiceLocator.cs
+++ src/Placebo.Injection/IServiceLocator.cs
@@ -17,5 +17,17 @@ namespace Placebo.Injection
 		/// <param name="serviceType">Type of the service.</param>
 		/// <returns>Collection of objects implementing interface.</returns>
 		IEnumerable ResolveAll(Type serviceType);
+
+		/// <summary>Registers the factory method.</summary>
+		/// <param name="serviceType">Type of the service.</param>
+		/// <param name="name">The name (optional).</param>
+		/// <param name="factory">The factory method.</param>
+		/// <returns>This resolver.</returns>
+		IServiceLocator Register(Type serviceType, string name, Func<IServiceLocator, object> factory);
+
+		/// <summary>Spawns child resolver.</summary>
+		/// <returns>Child resolver.</returns>
+		IServiceLocator Spawn();
+
 	}
 }
